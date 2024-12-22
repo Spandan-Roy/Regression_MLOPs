81a5b6ca0d9c1311a30aefba3feafdc06701d783
Spandan-Roy <spandan.roy.stats@gmail.com>
2024-12-22 13:02:36 +0530

    Data Trasnformation done

--- src/logger.py
+++ src/logger.py
@@ -10,5 +10,5 @@ LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lneno)d %(name)s - %(levelname)s  - %(message)s",
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s  - %(message)s",
     level=logging.INFO)
